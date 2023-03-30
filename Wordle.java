25ed7a6599f46b18fdfbdbd5b17b125d03e3368e
SpencerA264 <114941351+SpencerA264@users.noreply.github.com>
2023-03-30 12:44:16 -0400

    Create Wordle.java

---
+++ Wordle.java
@@ -0,0 +1,15 @@
+public class Wordle {
+  private final String word; 
+  private char[26] alphabet;
+
+  public Wordle() {
+    int counter;
+    for(char c = 'A'; c <= 'Z'; ++c) {
+      alphabet[counter]=c;
+      counter++;
+    }
+  }
+  
+  public int guess(
+  
+}
